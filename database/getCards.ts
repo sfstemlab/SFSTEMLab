74eca8edf960f0eececc53cff4a10c142d27bbaa
august-white <142743311+august-white@users.noreply.github.com>
2024-12-15 11:23:57 -0800

    commented getCards out

--- database/getCards.ts
+++ database/getCards.ts
@@ -1,41 +1,41 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const db = new PrismaClient(); 
+// const db = new PrismaClient(); 
 
-export async function getCards() {
-    try {
-        const cards = await db.card.findMany({
-            select: {
-                id: true,
-                name: true,
-                cmc: true,
-                rarity: true,
-                cardSetId: true
-            }
-        })
-        return cards
-    } catch (error:any) {
-        throw new Error(`Failed to fetch cards: ${error.message}`)
-    }
-}
+// export async function getCards() {
+//     try {
+//         const cards = await db.card.findMany({
+//             select: {
+//                 id: true,
+//                 name: true,
+//                 cmc: true,
+//                 rarity: true,
+//                 cardSetId: true
+//             }
+//         })
+//         return cards
+//     } catch (error:any) {
+//         throw new Error(`Failed to fetch cards: ${error.message}`)
+//     }
+// }
 
-export async function getCardsById(id:number) {
-    try {
-        const card = await db.card.findMany({
-            where: { id },
-            select: {
-                id: true,
-                name: true,
-                cmc: true,
-                rarity: true,
-                cardSetId: true
-            }
-        })
-        if (!card) throw new Error('Card not found')
+// export async function getCardsById(id:number) {
+//     try {
+//         const card = await db.card.findMany({
+//             where: { id },
+//             select: {
+//                 id: true,
+//                 name: true,
+//                 cmc: true,
+//                 rarity: true,
+//                 cardSetId: true
+//             }
+//         })
+//         if (!card) throw new Error('Card not found')
 
-        return card
-    } catch (error:any) {
-        throw new Error(`Failed to fetch cards: ${error.message}`)
-    }
-}
+//         return card
+//     } catch (error:any) {
+//         throw new Error(`Failed to fetch cards: ${error.message}`)
+//     }
+// }
 
