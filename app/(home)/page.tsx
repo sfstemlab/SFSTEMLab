8156c8c576cc52ae4c149290e164efcc8c1025ae
august-white <142743311+august-white@users.noreply.github.com>
2025-06-03 17:08:46 -0700

    fixed styling

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -34,14 +34,14 @@ export default function Home() {
     //                 * Fun and engaging for all skill levels
     //     `.split('');
 
-    const desc2 = `Our experienced student mentors and professionals from the community
+    const desc2 = `Our student mentors and professionals from the community
         play a key role in teaching the workshops by sharing their expertise and
         guiding students through the activites. This
         environment provides students with valuable skills and teaches the
         importance of teamwork, problem-solving, and working wtih others
         toward a common goal.`
 
-    const desc3 = `SF STEM Lab's workshops are entirely free and open to all. We 
+    const desc3 = `Our workshops are entirely free and open to all. We 
     are commited to ensuring that students from all backgrounds have 
     the opportunity to explore and create with STEM. SF STEM Lab aims 
     to make high-quality STEM education available to a wider audience, 
@@ -69,52 +69,68 @@ export default function Home() {
         >
             <Navbar />
             <img
-                src="@/../images/HomePage_HeroSection.png"
+                src="/images/HomePage_HeroSection.png"
                 alt="Home Page Hero Section"
                 width={1190}
                 className="hero-image"
             />
             <PageTitle title="Welcome to the SF STEM Lab!" />
-            <div className="main-section grid grid-cols-1 lg:grid-cols-2 gap-12">
-                <div className="flex flex-col items-center justify-center">
-                    <p className="text-4xl font-extrabold text-center mb-3 text-white">
-                        Explore Hands-on STEM Workshops
-                    </p>
-                    {/* {desc1.map((ln, idx) => (
-                        <p className="text-xl text-left indent-10 mt-4" key={idx}>
-                            {ln}
+            <div className="main-section  md:p-24">
+                <div className="flex items-center justify-between md:space-x-8 mb-12">
+                    <div className="flex flex-col justify-center">
+                        <p className="text-4xl font-extrabold text-center md:text-left mb-3 text-white">
+                            Explore Hands-on STEM Workshops
                         </p>
-                    ))} */}
-                    <p className="text-xl text-center">{desc1}</p>
-                    <LinkButton href="/events" title="Sign Up Now" />
+                        {/* {desc1.map((ln, idx) => (
+                            <p className="text-xl text-left indent-10 mt-4" key={idx}>
+                                {ln}
+                            </p>
+                        ))} */}
+                        <p className="text-xl text-center md:text-left w-full md:w-5/6">{desc1}</p>
+                        <LinkButton href="/events" title="Sign Up Now" />
+                    </div>
+                    <img
+                        src="/images/tools.png"
+                        className="rounded-full w-80 h-80 object-cover hidden md:block"
+                    />
+                </div>
+                <div className="flex items-center justify-between md:space-x-8 mb-12">
+                    <img
+                        src="/images/wiring.png"
+                        className="rounded-full w-80 h-80 object-cover hidden md:block"
+                    />
+                    <div className="flex flex-col items-center md:items-end">
+                        <p className="text-4xl font-extrabold text-center mb-3 text-white">
+                            Mentorship and Collaborative Learning
+                        </p>
+                        {/* {desc2.map((ln, idx) => (
+                            <p className="text-xl text-left indent-10 mt-4" key={idx}>
+                                {ln}
+                            </p>
+                        ))} */}
+                        <p className="text-xl text-center md:text-end w-full md:w-5/6">{desc2}</p>
+                        <LinkButton href="/about" title="Learn More" />
+                    </div>
                 </div>
 
-                <div className="flex flex-col items-center">
-                    <p className="text-4xl font-extrabold text-center mb-3 text-white">
-                        Mentorship and Collaborative Learning
-                    </p>
-                    {/* {desc2.map((ln, idx) => (
-                        <p className="text-xl text-left indent-10 mt-4" key={idx}>
-                            {ln}
+                <div className="flex items-center justify-between md:space-x-8 mb-12">
+                    <div className="flex flex-col items-center md:items-start">
+                        <p className="text-4xl font-extrabold text-center mb-3 text-white">
+                            Free and Accesible for All
                         </p>
-                    ))} */}
-                    <p className="text-xl text-center">{desc2}</p>
+                        {/* {desc3.map((ln, idx) => (
+                            <p className="text-xl text-left indent-10 mt-4" key={idx}>
+                                {ln}
+                            </p>
+                        ))} */}
+                        <p className="text-xl text-center md:text-left w-full md:w-5/6">{desc3}</p>
 
-                    <LinkButton href="/about" title="Learn More" />
-                </div>
-
-                <div className="flex flex-col items-center justify-center">
-                    <p className="text-4xl font-extrabold text-center mb-3 text-white">
-                        Free and Accesible for All
-                    </p>
-                    {/* {desc3.map((ln, idx) => (
-                        <p className="text-xl text-left indent-10 mt-4" key={idx}>
-                            {ln}
-                        </p>
-                    ))} */}
-                    <p className="text-xl text-center">{desc3}</p>
-
-                    <LinkButton href="/events" title="Donate" />
+                        <LinkButton href="/events" title="Donate" />
+                    </div>
+                    <img
+                        src="/images/homePage_image2.png"
+                        className="rounded-full w-80 h-80 object-cover hidden md:block"
+                    />
                 </div>
 
                 {/* <Circle
