6649e9765cf00366c4ae52247b9e37687c6bbff8
august-white <142743311+august-white@users.noreply.github.com>
2025-06-06 16:26:25 -0700

    added some pictures

--- app/signup/[slug]/page.tsx
+++ app/signup/[slug]/page.tsx
@@ -37,8 +37,51 @@ const SignupPage = () => {
                 <h1 className="text-brand text-center font-extrabold text-2xl mb-2">
                     Thank you for signing up for the {event}!
                 </h1>
-                <div className="flex flex-col justify-center items-center px-8 space-y-4">
-                    Form stuff
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                    <div className="flex flex-col">
+                        <h1>How did you hear about us?</h1>
+                        <textarea className='bg-brand rounded-md'></textarea>
+                    </div>
+                    <div className="flex flex-col">
+                        <h1>Why did you decide to attend this event?</h1>
+                        <textarea></textarea>
+                    </div>
+                    <div className="flex flex-col">
+                        <h1>What grade are you in?</h1>
+                        <input type="number"></input>
+                    </div>
+                    <div className="flex flex-col">
+                        <h1>What are your preferred pronouns?</h1>
+                        <select name="pronouns" required>
+                            <option value="" disabled selected>
+                                Select your pronouns
+                            </option>
+                            <option value="she/her">She/Her</option>
+                            <option value="he/him">He/Him</option>
+                            <option value="they/them">They/Them</option>
+                            <option value="she/they">She/They</option>
+                            <option value="he/they">He/They</option>
+                            <option value="they/she">They/She</option>
+                            <option value="they/he">They/He</option>
+                            <option value="xe/xem">Xe/Xem</option>
+                            <option value="ze/hir">Ze/Hir</option>
+                            <option value="it/its">It/Its</option>
+                            <option value="no-answer">Prefer not to answer</option>
+                            <option value="other">Other (please specify below)</option>
+                        </select>
+                        <input
+                            type="text"
+                            value="custom_pronouns"
+                            placeholder="If your pronouns don't fit any of the above options, select 'Other' and specify here" 
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <h1>What school do you go to?</h1>
+                        <input 
+                            type='text'
+                            value='school'
+                        />
+                    </div>
                 </div>
             </div>
         </div>
