995c6a99049e9122422b9ff97089d4a2bca1d937
august-white <142743311+august-white@users.noreply.github.com>
2025-07-09 15:01:33 -0700

    fixed stripe

---
+++ lib/stripe.ts
@@ -0,0 +1,14 @@
+import { loadStripe } from '@stripe/stripe-js'
+import Stripe from 'stripe'
+
+
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || "";
+
+if (!process.env.STRIPE_SECRET_KEY) {
+    console.log(`env variable not found: STRIPE_SECRET_KEY`)
+}
+
+export const stripe = new Stripe(STRIPE_SECRET_KEY, {
+    apiVersion: '2025-06-30.basil'
+})
